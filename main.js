3ee0641b19ee150fc21d7bd081b31ac4431117ba
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2024-12-08 12:54:44 -0400

    adding js file

---
+++ main.js
@@ -0,0 +1,13 @@
+//Block format function
+
+// function boolToWord( bool ){
+//   //...
+//   if (bool === true) {
+//     return "Yes"
+//   } else {
+//     return "No"
+//   }
+// }
+
+//Use the ternary to make a conitional within the arrow function
+const boolToWord = bool => bool === true ? "Yes" : "No";
